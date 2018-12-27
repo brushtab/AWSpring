491aeb8857ceb3255be8a202b6169e3100ab2b32
brushtab <brushtab@gmail.com>
2018-12-27 17:15:53 +0400

    Added spring

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,27 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private static final String templatePOST = "You are the best, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(path="/greeting", method=RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping(path="/greeting", method=RequestMethod.POST)
+    public Greeting YouBest(@RequestParam(value="name", defaultValue="Eugene Efimenko") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(templatePOST, name));
+    }
+}
